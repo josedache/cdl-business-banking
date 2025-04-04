d9a350654af8caa39f806f16249210e608702605
Sylvernus Akubo <akubosylvernus@gmail.com>
2025-04-04 17:30:16 +0100

    update'

--- src/modules/dashboard/pages/DashboardAccountSetup.tsx
+++ src/modules/dashboard/pages/DashboardAccountSetup.tsx
@@ -28,6 +28,7 @@ import { getTextFieldProps } from "utils/formik/get-text-field-props";
 import { LoadingButton } from "@mui/lab";
 import NumberTextField from "components/NumberTextField";
 import SecuredDataBadge from "components/SecuredDataBadge";
+import { ONBOARDING_STEPS } from "../enums/onboardingStepsEnum";
 
 function DashboardAccountSetup() {
   const { enqueueSnackbar } = useSnackbar();
@@ -75,237 +76,337 @@ function DashboardAccountSetup() {
     },
   });
 
-  const step1 = (
-    <Fragment key={0}>
-      <form onSubmit={formik.handleSubmit}>
-        <div className="p-6">
-          <ButtonBase
-            disableRipple
-            className="flex items-center gap-2"
-            onClick={() => stepper.previous()}
-          >
-            <Icon icon="weui:back-filled" fontSize={20} />
-            <Typography>Go back</Typography>
-          </ButtonBase>
-        </div>
+  const steps = [
+    {
+      title: "Provide NIN",
+      key: ONBOARDING_STEPS.NIN,
+      content: (
+        <Fragment key={0}>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="p-6">
+              <ButtonBase
+                disableRipple
+                className="flex items-center gap-2"
+                onClick={() => stepper.previous()}
+              >
+                <Icon icon="weui:back-filled" fontSize={20} />
+                <Typography>Go back</Typography>
+              </ButtonBase>
+            </div>
 
-        <Divider />
-        <div className="px-6 pt-4 pb-8">
-          <Typography variant="h5" className="">
-            Provide NIN
-          </Typography>
-          <Typography className=" text-neutral-500">
-            We need your NIN to confirm your identity and keep your account
-            secure
-          </Typography>
-          <NumberTextField
-            freeSolo
-            fullWidth
-            slotProps={{
-              input: {
-                inputProps: {
-                  maxLength: 11,
+            <Divider />
+            <div className="px-6 pt-4 pb-8">
+              <Typography variant="h5" className="">
+                Provide NIN
+              </Typography>
+              <Typography className=" text-neutral-500">
+                We need your NIN to confirm your identity and keep your account
+                secure
+              </Typography>
+              <NumberTextField
+                freeSolo
+                fullWidth
+                slotProps={{
+                  input: {
+                    inputProps: {
+                      maxLength: 11,
+                    },
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Icon
+                          icon="hugeicons:security-lock"
+                          width="24"
+                          height="24"
+                          className="text-neutral-950"
+                        />
+                      </InputAdornment>
+                    ),
+                  },
+                }}
+                label="NIN (National Identification Number)"
+                placeholder="19392398293"
+                className="mt-10"
+                {...getTextFieldProps(formik, "nin")}
+              />
+              <FormHelperText>
+                Enter your 11-digit NIN as it appears on your National ID slip.
+              </FormHelperText>
+            </div>
+            <Divider />
+            <div className="px-4 pb-4">
+              <LoadingButton
+                variant="gradient"
+                type="submit"
+                fullWidth
+                // disabled={!formik.isValid || !formik.dirty}
+                size="large"
+                loading={formik.isSubmitting}
+                loadingPosition="end"
+                endIcon={<></>}
+                className="my-5"
+              >
+                Verify NIN
+              </LoadingButton>
+
+              <div className="flex items-center justify-center gap-2">
+                <SecuredDataBadge />
+              </div>
+            </div>
+          </form>
+        </Fragment>
+      ),
+      verified: true,
+      hasStepper: true,
+    },
+    {
+      title: "Provide BVN",
+      key: ONBOARDING_STEPS.BVN,
+      content: (
+        <form onSubmit={formik.handleSubmit}>
+          <div>{renderStepper()}</div>
+          <div className="p-6">
+            <ButtonBase
+              disableRipple
+              className="flex items-center gap-2"
+              onClick={() => stepper.previous()}
+            >
+              <Icon icon="weui:back-filled" fontSize={20} />
+              <Typography>Go back</Typography>
+            </ButtonBase>
+          </div>
+
+          <Divider />
+          <div className="px-6 pt-4 pb-8">
+            <Typography variant="h5" className="">
+              Provide BVN
+            </Typography>
+            <Typography className=" text-neutral-500">
+              Your BVN helps us link your bank account and unlock transactions.
+            </Typography>
+            <NumberTextField
+              freeSolo
+              fullWidth
+              slotProps={{
+                input: {
+                  inputProps: {
+                    maxLength: 11,
+                  },
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Icon
+                        icon="hugeicons:security-lock"
+                        width="24"
+                        height="24"
+                        className="text-neutral-950"
+                      />
+                    </InputAdornment>
+                  ),
                 },
-                startAdornment: (
-                  <InputAdornment position="start">
+              }}
+              label="BVN (Bank Verification Number)"
+              placeholder="19392398293"
+              className="mt-10"
+              {...getTextFieldProps(formik, "nin")}
+            />
+            <FormHelperText>
+              We use it only for verification and never share your data
+            </FormHelperText>
+          </div>
+          <Divider />
+          <div className="px-4 pb-4">
+            <LoadingButton
+              variant="gradient"
+              type="submit"
+              fullWidth
+              // disabled={!formik.isValid || !formik.dirty}
+              size="large"
+              loading={formik.isSubmitting}
+              loadingPosition="end"
+              endIcon={<></>}
+              className="my-5"
+            >
+              Continue
+            </LoadingButton>
+
+            <div className="flex items-center justify-center gap-2">
+              <SecuredDataBadge />
+            </div>
+          </div>
+        </form>
+      ),
+      verified: false,
+      hasStepper: true,
+    },
+    {
+      title: "Business Details",
+      key: ONBOARDING_STEPS.BUSINESS,
+      content: (
+        <Fragment key={2}>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="p-6">
+              <ButtonBase
+                disableRipple
+                className="flex items-center gap-2"
+                onClick={() => stepper.previous()}
+              >
+                <Icon icon="weui:back-filled" fontSize={20} />
+                <Typography>Go back</Typography>
+              </ButtonBase>
+            </div>
+
+            <Divider />
+            <div className="px-6 pt-4 pb-8">
+              <Typography variant="h5" className="">
+                What type of business do you own?{" "}
+              </Typography>
+              <Typography className=" text-neutral-500">
+                Let’s confirm your business type to give you the best experience
+              </Typography>
+
+              <div className="grid grid-cols-1 gap-4 mt-6">
+                {[
+                  {
+                    icon: "hugeicons:building-03",
+                    title: "Business registered with CAC",
+                    description: "You’ll need your CAC number to proceed",
+                  },
+                  {
+                    icon: "solar:document-bold",
+                    title: "Business not yet registered with CAC",
+                    description: "You’ll provide a few details to continue",
+                  },
+                ].map(({ icon, title, description, ...rest }) => (
+                  <CardActionArea
+                    key={title}
+                    {...rest}
+                    className="flex gap-3 py-4 w-full  rounded-lg"
+                  >
+                    <Paper className="rounded-full bg-[#FFF3EE] border-1 border-[#FECBB9] p-3 w-fit">
+                      <Icon
+                        className="text-[#C53D0D]"
+                        icon={icon}
+                        width="24"
+                        height="24"
+                      />
+                    </Paper>
+                    <div className="flex-1">
+                      <Typography className="font-medium text-neutral-900">
+                        {title}
+                      </Typography>
+                      <Typography className="text-neutral-500 font-medium mt-2">
+                        {description}
+                      </Typography>
+                    </div>
                     <Icon
-                      icon="hugeicons:security-lock"
+                      icon="icon-park-outline:right"
                       width="24"
                       height="24"
-                      className="text-neutral-950"
                     />
-                  </InputAdornment>
-                ),
-              },
-            }}
-            label="NIN (National Identification Number)"
-            placeholder="19392398293"
-            className="mt-10"
-            {...getTextFieldProps(formik, "nin")}
-          />
-          <FormHelperText>
-            Enter your 11-digit NIN as it appears on your National ID slip.
-          </FormHelperText>
-        </div>
-        <Divider />
-        <div className="px-4 pb-4">
-          <LoadingButton
-            variant="gradient"
-            type="submit"
-            fullWidth
-            // disabled={!formik.isValid || !formik.dirty}
-            size="large"
-            loading={formik.isSubmitting}
-            loadingPosition="end"
-            endIcon={<></>}
-            className="my-5"
-          >
-            Verify NIN
-          </LoadingButton>
-
-          <div className="flex items-center justify-center gap-2">
-            <SecuredDataBadge />
-          </div>
-        </div>
-      </form>
-    </Fragment>
-  );
-
-  const step2 = (
-    <form onSubmit={formik.handleSubmit}>
-      <div className="p-6">
-        <ButtonBase
-          disableRipple
-          className="flex items-center gap-2"
-          onClick={() => stepper.previous()}
-        >
-          <Icon icon="weui:back-filled" fontSize={20} />
-          <Typography>Go back</Typography>
-        </ButtonBase>
-      </div>
-
-      <Divider />
-      <div className="px-6 pt-4 pb-8">
-        <Typography variant="h5" className="">
-          Provide BVN
-        </Typography>
-        <Typography className=" text-neutral-500">
-          Your BVN helps us link your bank account and unlock transactions.
-        </Typography>
-        <NumberTextField
-          freeSolo
-          fullWidth
-          slotProps={{
-            input: {
-              inputProps: {
-                maxLength: 11,
-              },
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Icon
-                    icon="hugeicons:security-lock"
-                    width="24"
-                    height="24"
-                    className="text-neutral-950"
-                  />
-                </InputAdornment>
-              ),
-            },
-          }}
-          label="BVN (Bank Verification Number)"
-          placeholder="19392398293"
-          className="mt-10"
-          {...getTextFieldProps(formik, "nin")}
-        />
-        <FormHelperText>
-          We use it only for verification and never share your data
-        </FormHelperText>
-      </div>
-      <Divider />
-      <div className="px-4 pb-4">
-        <LoadingButton
-          variant="gradient"
-          type="submit"
-          fullWidth
-          // disabled={!formik.isValid || !formik.dirty}
-          size="large"
-          loading={formik.isSubmitting}
-          loadingPosition="end"
-          endIcon={<></>}
-          className="my-5"
-        >
-          Continue
-        </LoadingButton>
-
-        <div className="flex items-center justify-center gap-2">
-          <SecuredDataBadge />
-        </div>
-      </div>
-    </form>
-  );
-  const step3 = (
-    <Fragment key={2}>
-      <form onSubmit={formik.handleSubmit}>
-        <div className="p-6">
-          <ButtonBase
-            disableRipple
-            className="flex items-center gap-2"
-            onClick={() => stepper.previous()}
-          >
-            <Icon icon="weui:back-filled" fontSize={20} />
-            <Typography>Go back</Typography>
-          </ButtonBase>
-        </div>
-
-        <Divider />
-        <div className="px-6 pt-4 pb-8">
-          <Typography variant="h5" className="">
-            What type of business do you own?{" "}
-          </Typography>
-          <Typography className=" text-neutral-500">
-            Let’s confirm your business type to give you the best experience
-          </Typography>
-
-          <div className="grid grid-cols-1 gap-4 mt-6">
-            {[
-              {
-                icon: "hugeicons:building-03",
-                title: "Business registered with CAC",
-                description: "You’ll need your CAC number to proceed",
-              },
-              {
-                icon: "solar:document-bold",
-                title: "Business not yet registered with CAC",
-                description: "You’ll provide a few details to continue",
-              },
-            ].map(({ icon, title, description, ...rest }) => (
-              <CardActionArea
-                key={title}
-                {...rest}
-                className="flex gap-3 py-4 w-full  rounded-lg"
+                  </CardActionArea>
+                ))}
+              </div>
+            </div>
+          </form>
+        </Fragment>
+      ),
+      verified: false,
+      hasStepper: true,
+    },
+    {
+      title: "Setup PIN",
+      key: ONBOARDING_STEPS.PIN_SETUP,
+      content: (
+        <Fragment key={3}>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="p-6">
+              <ButtonBase
+                disableRipple
+                className="flex items-center gap-2"
+                onClick={() => stepper.previous()}
               >
-                <Paper className="rounded-full bg-[#FFF3EE] border-1 border-[#FECBB9] p-3 w-fit">
-                  <Icon
-                    className="text-[#C53D0D]"
-                    icon={icon}
-                    width="24"
-                    height="24"
-                  />
-                </Paper>
-                <div className="flex-1">
-                  <Typography className="font-medium text-neutral-900">
-                    {title}
-                  </Typography>
-                  <Typography className="text-neutral-500 font-medium mt-2">
-                    {description}
-                  </Typography>
-                </div>
-                <Icon icon="icon-park-outline:right" width="24" height="24" />
-              </CardActionArea>
-            ))}
-          </div>
-        </div>
-      </form>
-    </Fragment>
-  );
+                <Icon icon="weui:back-filled" fontSize={20} />
+                <Typography>Go back</Typography>
+              </ButtonBase>
+            </div>
 
-  const steps = [
-    "Provider NIN",
-    "Provide BVN",
-    "Business Details",
-    "Setup Pin",
+            <Divider />
+            <div className="px-6 pt-4 pb-8">
+              <Typography variant="h5" className="">
+                What type of business do you own?{" "}
+              </Typography>
+              <Typography className=" text-neutral-500">
+                Let’s confirm your business type to give you the best experience
+              </Typography>
+
+              <div className="grid grid-cols-1 gap-4 mt-6">
+                {[
+                  {
+                    icon: "hugeicons:building-03",
+                    title: "Business registered with CAC",
+                    description: "You’ll need your CAC number to proceed",
+                  },
+                  {
+                    icon: "solar:document-bold",
+                    title: "Business not yet registered with CAC",
+                    description: "You’ll provide a few details to continue",
+                  },
+                ].map(({ icon, title, description, ...rest }) => (
+                  <CardActionArea
+                    key={title}
+                    {...rest}
+                    className="flex gap-3 py-4 w-full  rounded-lg"
+                  >
+                    <Paper className="rounded-full bg-[#FFF3EE] border-1 border-[#FECBB9] p-3 w-fit">
+                      <Icon
+                        className="text-[#C53D0D]"
+                        icon={icon}
+                        width="24"
+                        height="24"
+                      />
+                    </Paper>
+                    <div className="flex-1">
+                      <Typography className="font-medium text-neutral-900">
+                        {title}
+                      </Typography>
+                      <Typography className="text-neutral-500 font-medium mt-2">
+                        {description}
+                      </Typography>
+                    </div>
+                    <Icon
+                      icon="icon-park-outline:right"
+                      width="24"
+                      height="24"
+                    />
+                  </CardActionArea>
+                ))}
+              </div>
+            </div>
+          </form>
+        </Fragment>
+      ),
+      verified: false,
+      hasStepper: true,
+    },
   ];
+
+  const currentStep = steps.find((step) => step.key === stepper.step);
 
   return (
     <div className="w-full max-w-[600px] mx-auto">
-      <Stepper activeStep={stepper.step} connector={<CustomSVGConnector />}>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel StepIconComponent={QontoStepIcon}>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      <Paper className="mt-5">{[step1, step2, step3][stepper.step]}</Paper>
+      <Paper className="mt-5">
+        <Stepper
+          activeStep={stepper.step}
+          connector={<CustomSVGConnector />}
+          className="mb-5"
+        >
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel StepIconComponent={QontoStepIcon}>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        {steps.map((step) => step.content)}
+      </Paper>
     </div>
   );
 }
@@ -313,6 +414,13 @@ function DashboardAccountSetup() {
 export default DashboardAccountSetup;
 
 export const Component = DashboardAccountSetup;
+
+export const ONBOARDING_STEP_ = {
+  NIN: 1,
+  BVN: 2,
+  BUSINESS: 3,
+  PIN_SETUP: 4,
+};
 
 const CustomSVGConnector = styled(StepConnector)(() => ({
   [`&.${stepConnectorClasses.alternativeLabel}`]: {
