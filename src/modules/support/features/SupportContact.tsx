773e6e0262aa8dbdb14e833fdde2659b2ec38516
Joseph Edache <josedache0@gmail.com>
2025-05-20 17:36:01 +0100

    feat: customer support dialog

--- src/modules/support/features/SupportContact.tsx
+++ src/modules/support/features/SupportContact.tsx
@@ -12,9 +12,12 @@ import {
 import DialogTitleXCloseButton from "components/DialogTitleXCloseButton.tsx";
 import useToggle from "hooks/use-toggle.ts";
 import { Icon as Iconify } from "@iconify/react";
+import useAuthUser from "hooks/use-auth-user.ts";
 
 function SupportContact(props: SupportContactProps) {
   const { children, onClose, ...restProps } = props;
+
+  const authUser = useAuthUser();
 
   const [isOpen, toggleOpen, setOpen] = useToggle();
 
@@ -28,24 +31,36 @@ function SupportContact(props: SupportContactProps) {
       <Dialog open={isOpen} fullWidth maxWidth="xs" {...restProps}>
         <DialogTitleXCloseButton onClose={handleClose} />
         <DialogContent>
-          <div className='text-center mb-6 space-y-2'>
+          <div className="text-center mb-6 space-y-2">
             <Typography variant="h5">Customer Support</Typography>
             <Typography variant="body1">
-              Hey Segun, We’re here to help! Choose how to reach us.
+              Hey{" "}
+              <span className="capitalize">
+                {authUser?.info?.firstName?.toLowerCase()}
+              </span>
+              , We’re here to help! Choose how to reach us.
             </Typography>
           </div>
           <Paper variant="outlined" className="bg-neutral-100 p-4 space-y-2">
             {[
-              { label: "Email", icon: "line-md:email-filled" },
+              {
+                label: "Email",
+                icon: "line-md:email-filled",
+                component: "a",
+                href: "mailto:cdlbusiness@creditdirect.ng ",
+              },
               {
                 label: "Phone number",
                 icon: "mingcute:phone-call-fill",
+                component: "a",
+                href: "tel:02014482225 ",
               },
               { label: "Live chat", icon: "fluent:chat-24-filled" },
-            ].map(({ label, icon }) => (
+            ].map(({ label, icon, ...props }) => (
               <ButtonBase
                 key={label}
                 className="flex items-center gap-4 text-left p-2 w-full"
+                {...props}
               >
                 <IconButton
                   disableRipple
